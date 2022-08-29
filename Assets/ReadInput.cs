35316861942ac6e01992abd3b38a15f39af0a40f
Boganabi <logan.ashbaugh0001@gmail.com>
2022-08-29 15:16:44 -0700

    first draft of cs file done and py file api started

--- Assets/ReadInput.cs
+++ Assets/ReadInput.cs
@@ -2,13 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
 public class ReadInput : MonoBehaviour
 {
     private string input;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Task.Run(() => RunAsync());
     }
 
     // Update is called once per frame
@@ -21,5 +27,28 @@ public class ReadInput : MonoBehaviour
     {
         input = s;
         Debug.Log(input);
+        
     }
+
+    static async Task RunAsync()
+    {
+        using (var client = new HttpClient())
+        {
+            client.BaseAddress = new Uri("http://127.0.0.1:5000/");
+            var content = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("input", "Hello")
+            });
+            var result = await client.PostAsync("", content);
+            string resultContent = await result.Content.ReadAsStringAsync();
+            Debug.Log(resultContent);
+        }
+    }
+}
+
+class Product
+{
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public string Category { get; set; }
 }
